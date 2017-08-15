03fcb2c63cac2970088b5c2d9c6e46b80ae4b040
Eric Hankinson <eric.hankinson@leandog.com>
2017-08-15 12:21:23 -0400

    Some visual & markup cleanup to remove friction on first time testers

--- app/controllers/agency_controller.rb
+++ app/controllers/agency_controller.rb
@@ -2,7 +2,7 @@ class AgencyController < ApplicationController
   skip_before_action :authorize, raise: false
 
   def index
-    @puppies = Puppy.paginate(page: params[:page], per_page: 4).order(:name)
+    @puppies = Puppy.order(:name)
   end
 
 end
