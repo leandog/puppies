4410908dfd402adf595291edb3b6ef2e88b2fdb8
Eric Hankinson <eric.hankinson@leandog.com>
2017-08-18 10:37:59 -0400

    Updated Contact form functionality to leverage it for testing later

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,15 @@ class ContactsController < ApplicationController
   def create
     @message = ContactMessage.new(contact_message_params)
 
-
+    respond_to do |format|
+      if @message.valid?
+        format.html { redirect_to(agency_url, :notice => 'Thank you for contacting us!') }
+        format.json { render :json => @message }
+      else
+        format.html { render :action => "new" }
+        format.json { render json: @message.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
 
